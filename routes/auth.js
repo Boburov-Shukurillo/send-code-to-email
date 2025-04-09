5a44b7a0afac8888b61bb654a4f9069a5d78f154
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2025-04-09 11:06:46 +0500

    empty

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { RegiterUser } = require("../controllers/UserController");
-const router = express.Router();
-
-router.post('/register', RegiterUser)
-
-module.exports = router
